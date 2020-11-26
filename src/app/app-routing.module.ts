b01ab97c639e8577765ea426e8744e741f1cc35f
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-11-26 05:39:51 -0300

    agrega implementacion de ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AportarContactComponent } from './aportar-contact/aportar-contact.component';
+import { AportarExploreComponent } from './aportar-explore/aportar-explore.component';
+import { AportarHomeComponent } from './aportar-home/aportar-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AportarHomeComponent},
+  { path: 'explore', component: AportarExploreComponent},
+  { path: 'contact', component: AportarContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
